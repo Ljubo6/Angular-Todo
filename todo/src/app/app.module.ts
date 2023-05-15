60a73b9868e512f1b65c9d16e1e570d21dc0b207
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2023-05-15 23:32:34 +0300

    install Angular/fire,firebase and set background image

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {environment} from "../environments/environment";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,10 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { CategoryComponent } from './category/category.component';
 import { TodoComponent } from './todo/todo.component';
+import {FormsModule} from "@angular/forms";
+import {AngularFireModule} from "@angular/fire/compat";
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +25,11 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- todo/src/environments/environment.development.ts
+++ todo/src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA7AVbzdrAbYA4VkNxR7cdm2aNR6FaDGuA",
+    authDomain: "todo-b5234.firebaseapp.com",
+    projectId: "todo-b5234",
+    storageBucket: "todo-b5234.appspot.com",
+    messagingSenderId: "67028520136",
+    appId: "1:67028520136:web:4c1a43c5f514ce848d29fd",
+    measurementId: "G-MWBCW1EGE0"
+  }
+};
